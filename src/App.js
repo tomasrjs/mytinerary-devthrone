c8925bced72859ebd159390a9000681aa1117a6b
Tomas Rojas <88802429+tomasrjs@users.noreply.github.com>
2022-08-18 19:09:55 -0400

    Hero (#3)
    * readme deleted
    
    * react proyect initialization
    
    * WelcomePage added
    
    * component WelcomePage created
    
    * styles WelcomePage created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import WelcomePage from './pages/WelcomePage';
+
+function App() {
+  return (
+    <>
+    <WelcomePage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/WelcomePage.css'
+
+export default function WelcomePage() {
+
+
+  return (
+    <>
+    <div className='App-container'>
+        <h1>MyTinerary</h1>
+        <p>Find your perfect trip, designed by insiders who know and love their cities!</p>
+        <button>Choose your destiny</button>
+    </div>
+    </>
+  )
+}
