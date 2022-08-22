f1a5dfe630fc5d78ce6f8d608cecc83736fafc33
tomasrjs <rojastomas649@gmail.com>
2022-08-22 18:09:24 -0400

    cities page created

---
+++ src/pages/CitiesPage.js
@@ -0,0 +1,10 @@
+import WebsiteLayout from "../layouts/WebsiteLayout";
+import React from 'react'
+
+export default function CitiesPage() {
+  return (
+    <WebsiteLayout>
+        <div>CitiesPage</div>
+    </WebsiteLayout>
+  )
+}
