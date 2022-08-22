29b47f0d1f65552fb28aa6020f86221e3f10ce14
emazou <estefania.mazou@gmail.com>
2022-08-22 12:09:28 -0500

    component CallToAction created

---
+++ src/components/CallToAction.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../styles/CallToAction.css'
+export default function CallToAction() {
+  return (
+    <a className='CallToAction-a' href='#'>Choose your destiny <span className='CallToAction-span'></span></a>
+  )
+}
