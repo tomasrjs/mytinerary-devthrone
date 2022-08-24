2ad8956c26c194f4fca12ce4b59447750740dcc1
tomasrjs <rojastomas649@gmail.com>
2022-08-23 21:57:02 -0400

    navbar modified and button added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import '../styles/Footer.css'
+import { Link as LinkRouter } from "react-router-dom"
+
 export default function Footer () {
+  const scroll = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
   return (
     <>
     <div className='Footer-container'>
-        <div className='Footer-container-nav'>
-            <p>Home</p>
-            <p>Cities</p>
-        </div>
+        <nav className='Footer-container-nav'>
+              <LinkRouter className='Footer-link' to='/'>Home</LinkRouter>
+              <LinkRouter className='Footer-link' to='/cities'>Cities</LinkRouter>
+              <LinkRouter className='Footer-link' to='/newCites'>New Cities</LinkRouter>
+        </nav>
         <div className='Footer-continer-socialmedia'>
             <a href="https://www.facebook.com/"><img src="/facebook.png" alt='social media' /></a>
             <a href="https://www.instagram.com/"><img src="/instagram.png" alt='social media' /></a>
             <a href="https://github.com/emazou/mytinerary-devthrone"><img src="/github.png" alt='social media' /></a>
         </div>
         <p>©2022 MyTinerary</p>
+        <button className='scroll' onClick={scroll}></button>
     </div>
     </>
   )
