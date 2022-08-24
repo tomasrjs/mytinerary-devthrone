65d90bda37f3151373829736750fd9b5947520e0
emazou <estefania.mazou@gmail.com>
2022-08-23 20:12:07 -0500

    import deleted

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import WebsiteLayout from '../layouts/WebsiteLayout'
 import CitiesCarousel from '../components/CitiesCarousel'
 import Hero from '../components/Hero'
 
 export default function WelcomePage() {
- 
-  return (  
-    <WebsiteLayout>
-      <Hero/>
-    <CitiesCarousel />
-    </WebsiteLayout>   
+
+  return (
+    <>
+      <Hero />
+      <CitiesCarousel />
+    </>
   )
 }
